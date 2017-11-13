74597f88da8613a1396c36aba0ce3821e616f502
Alexander Geenen <geenen124@gmail.com>
2017-11-13 14:59:14 +0100

    Add EasyDataset

---
+++ data_loader.py
@@ -0,0 +1,51 @@
+from __future__ import print_function
+import os
+import torch
+import pandas as pd
+import numpy as np 
+from torch.utils.data import DataSet, DataLoader
+import json
+import h5py
+
+
+class EasyDataset(DataSet):
+    def __init__(self, data_directory, training_file,
+                 image_mapping_file, image_feature_file):
+        self.image_features = np.asarray(
+                h5py.File(
+                    data_directory + image_feature_file,
+                    'r')['img_features'])
+
+        with open(data_directory + image_mapping_file, 'r') as f:
+            self.image_id_feature_mapping = json.load(f)['IR_imgid2id']
+
+        with open(data_directory + training_file, 'r') as f:
+            self.training_data = list(json.load(f).values())
+
+    def __len__(self):
+        return len(self.training_data)
+    
+    def __getitem__(self, item_index):
+        training_item_dict = self.training_data[item_index].copy()
+        image_features = dict()
+        for image_index, image_id in enumerate(training_item_dict['img_list']):
+            this_images_features = self.image_features[
+                    self.image_id_feature_mapping[str(image_id)]]
+            image_features[image_id] = this_images_features
+            # Also add this as the target image features if it corresponds
+            if image_index == training_item_dict['target']:
+                training_item_dict['target_img_features'] = this_images_features
+
+        training_item_dict['img_features'] = image_features
+
+        return training_item_dict
+
+
+
+if __name__ == '__main__':
+    easy_dataset = EasyDataset(
+            data_directory="./data",
+            training_file="IR_train_easy.json",
+            image_mapping_file="IR_image_features2id.json",
+            image_feature_file="IR_image_features.h5",
+            )
